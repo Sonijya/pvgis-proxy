1fb321bf89bc64a6cc6598b1c555e2bd472928b3
Sonijya <sonja.bless@gmx.de>
2025-06-28 17:04:57 +0200

    Update pvgis.js

--- api/pvgis.js
+++ api/pvgis.js
@@ -1,6 +1,11 @@
+
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   res.setHeader('Access-Control-Allow-Origin', '*'); // 🔥 das ist der wichtige Fix
 
   const { lat, lon, year } = req.query;
